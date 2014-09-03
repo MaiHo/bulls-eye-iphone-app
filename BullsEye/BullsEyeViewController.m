7c342e1b76364f394aa6297f5646532debd4a1b0
Mai Ho <maiho@Mais-MacBook-Pro.local>
2014-09-02 22:54:19 -0700

    Added the general layout to the single view app.

--- BullsEye/BullsEyeViewController.m
+++ BullsEye/BullsEyeViewController.m
@@ -13,17 +13,37 @@
 @end
 
 @implementation BullsEyeViewController
+{
+    int _currentValue;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    _currentValue = 50;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)showAlert
+{
+    NSString *message = [NSString stringWithFormat: @"The value of the slider is: %d", _currentValue];
+    UIAlertView *alertView = [[UIAlertView alloc]
+                              initWithTitle:@"Hello, World"
+                              message:message
+                              delegate:nil
+                              cancelButtonTitle:@"Awesome" otherButtonTitles:nil];
+  
+    [alertView show];
+}
+
+- (IBAction)sliderMoved:(UISlider *)slider {
+    _currentValue = lroundf(slider.value);
 }
 
 @end
